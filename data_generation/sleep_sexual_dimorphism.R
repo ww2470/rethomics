3be64ff0a8ddf513adcbf1d034a16531a7024de8
Quentin Geissmann <qgeissmann@gmail.com>
2015-07-14 21:03:25 +0100

    Fixes #10
    Former-commit-id: 405266c58e9b233bb27b8748a22b21430aab531e [formerly d1fd75832acd039ebe27f21bcac7c26dc601fe86]
    Former-commit-id: 83761e18aafedc25cad6264c27b3af1f4eb2d537

---
+++ data_generation/sleep_sexual_dimorphism.R
@@ -0,0 +1,26 @@
+rm(list=ls()); graphics.off()
+library(rethomics)
+MY_RESULT_DIR <- "/data/psv_results"
+OUT_NAME <- "sleep_sexual_dimorphism.RData"
+query <- fread("sleep_sexual_dimorphism.csv")
+path_query <- fetchPsvResultFiles(MY_RESULT_DIR, query)
+dt <- loadPsvData(
+  # What files/conditions to fecth
+  path_query, 
+  # We apply directly the function to annotate sleep event. This will make the data smaller (1point/10s)
+  FUN=sleepAnnotation, 
+  # Importantly, we define the refence hour (zt0) at 09:00, GMT (i.e. 10:00 in British Summer Time)
+  reference_hour=9.0, 
+  cache=TRUE,
+  n_cores=4
+)
+
+dt$w <- NULL
+dt$h <- NULL
+dt$phi <- NULL
+dt$xor_dist <- NULL
+dt$mlog_L_x1000 <- NULL
+xy_dist_log10x1000 <- NULL
+
+sleep_sexual_dimorphism <- dt
+save(sleep_sexual_dimorphism,file=OUT_NAME,compress = "xz")
