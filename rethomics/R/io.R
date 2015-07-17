819603454e2c0f3573767e04ab892025b35d0e65
Quentin Geissmann <qgeissmann@gmail.com>
2015-07-17 15:32:12 +0100

    fixes #13

--- rethomics/R/io.R
+++ rethomics/R/io.R
@@ -300,6 +300,8 @@ NULL
 #' This function is used to load data from DAM2 devices as a data.table.
 #'
 #' @param FILE the name of the input file.
+#' @param start_date the starting date formated as "yyyy-mm-dd" or "yyyy-mm-dd_hh-mm-ss"
+#' @param stop_date the last day of the experiment. Same format as \code{start_date}
 #' @param tz the time zone of the computer saving the file. By default, \code{tz} is taken from the computer running this function
 #' @return a data table with an activity (number of beam crosses) variable, a region_id (channel) variable and a posix time stamp.
 #' @examples
@@ -317,18 +319,54 @@ NULL
 #' print(conditions)
 #' }
 #' @seealso \code{\link{loadMetaData}} To display global informations about the experiment.
-#' @export
-loadDAMFile <- function(FILE, tz = ""){
-  ### hardcodded constants
-  DAM_COL_TYPES <- c(
-    "integer", "character", "character",
-    #rep(NULL, 7), ## these columns are irrelevant, NULL means we will discard them straight away
-    rep("double", 32)
-  )
+loadDAMFile <- function(FILE, start_date=-Inf, stop_date=+Inf, tz = ""){
+  # 1 load time stamps
+  dt <- fread(FILE, select=2:3, header = FALSE)
+  dt[,datetime:=paste(V2,V3, sep=" ")]
+  dt[,t:=as.POSIXct(strptime(datetime,"%d %b %y %H:%M:%S",tz=tz))]
+  min_date <- dateStrToPosix(start_date,tz)
+  max_date <- dateStrToPosix(stop_date,tz)
+  # if time is not in date, we add a day
+  ifelse(parseDateStr(stop_date)$has_time, max_date,max_date +hours(24))
+  if(max_date < min_date)
+    stop("`max_date` MUST BE greater than `min_date`")
+  valid_dt <- dt[,.(
+    valid = (t >= min_date & t < max_date),
+    idx = 1:.N,
+    t=t
+  )]
+  valid_dt <- valid_dt[valid ==T ]
   
+  first = min(valid_dt[,idx]) 
+  last = max(valid_dt[,idx])     
+  
+  #2 check time stamps
+  if(nrow(valid_dt) < 1){
+    stop("There is apparently no data in this range of dates")
+  }
+  
+  valid_dt[,diff_t := c(NA,diff(t))]
+  valid_dt <- na.omit(valid_dt)
+  sampling_periods <- valid_dt[,.(n=.N),by=diff_t]
+  if(nrow(sampling_periods) > 1){
+    warning("The sampling period is not always regular.
+            Some reads must have been skipped.")
+    #fixme show a table of samplig rates
+  }
+  
+  if(any(sampling_periods[,diff_t] == 0)){
+    stop("Some of the dates are repeated between successive measument.
+         This can happen if the recording computer change time (e.g. between winter and summer time)")
+  }
+  
+  if(any(sampling_periods[,diff_t] < 0)){
+    stop("Come measument appear to have been recorded 'before' previous measuments.
+         It looks as if the recording computer went back in time!")
+  }
+  # 3 actually load the file
   DAM_COL_NAMES <- c("idx", "day_month_year", "time", sprintf("channel_%02d", 1:32))
-  
-  dt_list <- fread(FILE, drop=4:10, header = FALSE)
+  dt_list <- fread(FILE, drop=4:10, header = FALSE,
+                   skip = first-1, nrows = last-first+1)
   setnames(dt_list,DAM_COL_NAMES)
   dt_list[,datetime:=paste(day_month_year,time, sep=" ")]
   dt_list[,t:=as.POSIXct(strptime(datetime,"%d %b %y %H:%M:%S",tz=tz))]
@@ -353,7 +391,6 @@ loadDAMFile <- function(FILE, tz = ""){
   out$variable <- NULL
   return(out)
 }
-
 NULL
 #' Retrieves sample/example data contained within in this package.
 #' 
@@ -517,7 +554,7 @@ listDailyDAMFiles <- function(result_dir){
   files_info$file <- NULL
   files_info
 }
-#' Retrieves DAM2 data from data that is saved daily
+#' Retrieves DAM2 data from daily saved files
 #' 
 #' Uses a query mechanism to get data from a DAM2 array. 
 #' This is useful when data has been saved, by day, in individual files for each monitor.
@@ -550,7 +587,7 @@ listDailyDAMFiles <- function(result_dir){
 #'  \item{\code{mm} and \code{dd}, }{the formatted month and day, respectively (e.g. mm=12 and dd=28).}
 #'  \item{\code{xyz}, }{the number of the monitor (e.g 003)}
 #' }
-
+#' @seealso \code{\link{queryDAMData}} to load data from a regular DAM2 file
 #' @export
 fetchDAMData <- function(result_dir,query, reference_hour=9.0, tz="BST", FUN=NULL, ...){
   q = copy(query)
@@ -565,7 +602,6 @@ fetchDAMData <- function(result_dir,query, reference_hour=9.0, tz="BST", FUN=NUL
   q[, experiment_id := paste(start_date,machine_id,sep="_")]
   
   foo <- function(d){
-    
     t0 <- unique(d[,start_date])
     t1 <- unique(d[,stop_date])
     mid <- unique(d[,machine_id])
@@ -611,7 +647,6 @@ fetchDAMData <- function(result_dir,query, reference_hour=9.0, tz="BST", FUN=NUL
   all_data
 }
 
-
 cacheResultFile <- function(all_paths,subdir="rethomic_file_cache"){
   src <- unique(all_paths)
 
@@ -627,5 +662,132 @@ cacheResultFile <- function(all_paths,subdir="rethomic_file_cache"){
   path_map <- data.table(src=all_paths)
   return(map[path_map][,dst])
 }
+NULL
+
+#' Retrieves DAM2 data from continuous file recording
+#' 
+#' Uses a query mechanism to get data from a DAM2 array. 
+#' This is usefuld when using the default behaviour of Trikinetics software
+#' where data is simply appended to a single long file per monitor.
+#' 
+#' @param query a formatted query used to request data (see detail).
+#' @param FUN an optional function to transform the data from each `region' (i.e. a data.table) immediately after is has been loaded. 
+#' @param ... extra arguments to be passed to \code{FUN}
+#' @return A data.table where every row is an individual measurement. That is an activity at a unique time (\code{t}) in a 
+#' unique channel (\code{region_id}), and from a unique result date/experiment (\code{experiment_id}).
+#' The time is expressed in seconds. For each different combination of \code{start_date} and \code{file} in the query, an
+#' individual \code{experiment_id} is generated.
+#' @details \code{query} must be a data.table.
+#' Conceptually, each row of the query describes the conditions in one channel (when \code{region_id} is specified), or in each monitor (when it is not).
+#' It should have  the following columns:
+#' \itemize{
+#'  \item{\code{path} }{ the location of your data file (e.g. `C:/User/me/Desktop/Monitor3.txt').}
+#'  \item{\code{start_date} }{ the first day of the requested experiment (e.g. `2014-12-28').}
+#'  \item{\code{stop_date} }{ the last day of the requested experiment (e.g. `2014-12-30').}
+#'  \item{\code{region_id} }{ the channel (between 1 and 32) in what the animal was in (e.g. `20'). This is an optional column. If not provided, all 32 channels are loaded with the same conditions.}
+#'  \item{\code{...} }{ arbitrary columns to associate conditions/treatments/genotypes/... to the previous columns}
+#' }
+#'
+#' @examples
+#' sample_file <- system.file('data/DAMfile.txt', package="rethomics")
+#' query = data.table(path=sample_file,
+#'                    # note the time (10:00) is added as reference time
+#'                  start_date="2015-07-02_10-00-00", 
+#'                  stop_date="2015-07-07",
+#'                  region_id=c(1:32),condition=rep(letters[1:2],each=16))
+#' print(query)
+#' dt <- queryDAMFiles(query)
+#' ethogramPlot(activity,dt,condition) + scale_x_continuous(breaks=0:10/2)
+#' dt <- queryDAMFiles(query,FUN= sleepDAMAnnotation)
+#' ethogramPlot(asleep,dt,condition) + scale_x_continuous(breaks=0:10/2)
+#' @seealso \code{\link{fetchDAMData}} to load DAM data that is saved by day
+#' @export
+queryDAMFiles <- function(query, FUN, ...){
+  tz=""
+  q <- copy(as.data.table(query))
+  cn <- colnames(q)
+ if(!("path" %in% cn & "start_date" %in% cn & "stop_date" %in% cn )){
+  stop("query MUST have at least thre columns names `path`, `start_date` and `stop_date`")
+ }
+  q[, experiment_id := paste(start_date,basename(path),sep="_")]
+  if(!"region_id" %in% cn)
+    q <- q[q[,.(region_id=1:32),by=experiment_id]]
+  setkey(q,experiment_id)
+  
+  wrapLoadDAM <- function(p,srtd,stpd,tz){
+    out <- loadDAMFile(p,srtd,stpd,tz=tz)
+    out[,t:= t]
+    out
+  }
+  
+  out <- unique(q)[,
+           wrapLoadDAM(path,start_date, stop_date,tz),
+           by=experiment_id]
+  
+  q$path <- NULL
+  setkeyv(out,c("experiment_id","region_id"))
+  
+  final_key <- copy(colnames(q))
+  
+  setkeyv(q,final_key)
+  q[, t0:=sapply(start_date,dateStrToPosix,tz)]
+  out <- merge(q,out)
+  
+  out[, t:=as.numeric(t-t0,units='secs')]
+  out$t0 <- NULL
+  
+  setkeyv(out,final_key)
+  
+  if(!is.null(FUN)){
+    out <- out[, FUN(.SD,...),by=key(out)]
+  }
+  out
+}
+NULL
 
 
+# valid_dates <- c('2015-05-04_10-00-00', '2015-05-04')
+# invalid_dates <- c('2015-05-04_25-00-00', '2015-05-34',
+#                   '2015-05-03 10:23:11')
+parseDateStr <- function(str, tz=''){
+  
+  pattern <- "^[0-9]{4}-[0-9]{2}-[0-9]{2}(_[0-9]{2}-[0-9]{2}-[0-9]{2}){0,1}$"
+  if(length(str) >1){
+    stop("Dates must be checked one by one, you are providing several dates")
+  }
+  match = grep(pattern, str)
+  if (length(match) != 1){
+    stop(sprintf("Date '%s' is not formated correctly.
+                 It must be either 'yyyy-mm-dd' or 'yyyy-mm-dd_hh-mm-ss'",str))
+  }
+  
+  out <- list()
+  
+  if(nchar(str) == 10){
+    date <- as.POSIXct(str, "%Y-%m-%d",tz=tz)
+    out$date <- date
+    out$has_time <- F
+  }
+  else{
+    date <- as.POSIXct(str, "%Y-%m-%d_%H-%M-%S",tz=tz)
+    out$date <- date
+    out$has_time <- T
+  }
+  if(is.na(date)){
+    stop(sprintf("Date '%s' seems to be formated correctly,
+                 but we cannot read it as a date. 
+                 Probably the numbers are wrong (e.g. 2015-30-02 does not exist)",str))
+  }
+  
+  out
+}
+
+
+dateStrToPosix <- function(date,tz="GMT"){
+  if(is.infinite(date))
+    return(date)
+  parseDateStr(date,tz)$date
+}
+
+# invalid_dates <- c('2015-05-04_25-00-00', '2015-05-34',
+#                   '2015-05-03 10:23:11')
