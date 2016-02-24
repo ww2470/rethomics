60c0a322183fc30b6b46ef35c3cfa2daf91facf0
Quentin Geissmann <qgeissmann@gmail.com>
2016-02-24 13:37:26 +0000

    verbose in travis tuto script
    Former-commit-id: c9757f36ffe8946054fdedb60985a912c744fb69 [formerly ff70b0c28f9b27d838e5ed7fd73b639652052630]
    Former-commit-id: 7c6b9d7fbad61f71f5e350d5000451dafa5b352b

--- tools/make_tuto.sh
+++ tools/make_tuto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GH_REPO="@github.com/USER/REPO.git"
+GH_REPO="@github.com/gilestrolab/rethomics.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 mkdir out
@@ -15,9 +15,13 @@ git config user.email "travis"
 
 # do useful work for gh-pages, for example convert README.md to index.html
 #pandoc ../README.md -o index.html
+echo 'making file `test`'
 echo "$TRAVIS_COMMIT" > test
+cat test
 
 # commit and push changes
 git add test
 git commit -m "test autocommit after $TRAVIS_COMMIT"
+echo "pushing to master"
 git push origin master
+echo "DONE"
